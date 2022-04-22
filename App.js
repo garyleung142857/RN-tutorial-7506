52285bf78f2c88772ef60580d8cc4607a14c0c87
garyleung142857 <garyleung142857@gmail.com>
2022-04-22 16:05:03 +0800

    functionalities and states

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
+import React from 'react'
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View, Alert } from 'react-native'
 import NumberBox from './components/NumberBox'
 import SumResult from './components/SumResult'
 import SumButton from './components/SumButton'
 
 export default function App() {
+  const [numArr, setNumArr] = React.useState([0, 0, 0])
+  const [sum, setSum] = React.useState(null)
+
+  const strToNum = (idx, v) =>{
+    let num = Number(v)
+    let tempArr = [...numArr]
+    tempArr[idx] = num
+    setNumArr(tempArr)
+    setSum(null)  // reseting the sum when user inputs new numbers
+  }
+
+  const onPressBtn = () => {
+    const arrSum = numArr[0] + numArr[1] + numArr[2]
+    if (isNaN(arrSum)){
+      Alert.alert('There is an error in your input.')
+    } else {
+      setSum(arrSum)
+    }
+  }
+
   return (
     <View style={styles.container}>
       <View style={{alignItems: 'flex-end'}}>
         <StatusBar style="auto" />
-        <NumberBox textStyle={styles.texts}/>
-        <NumberBox textStyle={styles.texts}/>
+        <NumberBox num={numArr[0]} onChange={v => strToNum(0, v)} textStyle={styles.texts}/>
+        <NumberBox num={numArr[1]} onChange={v => strToNum(1, v)} textStyle={styles.texts}/>
         <View style={{flexDirection: 'row'}}>
-          <SumButton textStyle={styles.texts}/>
-          <NumberBox textStyle={styles.texts}/>
+          <SumButton onPressBtn={onPressBtn} textStyle={styles.texts}/>
+          <NumberBox num={numArr[2]} onChange={v => strToNum(2, v)} textStyle={styles.texts}/>
         </View>
-        <SumResult result={0} textStyle={styles.texts}/>
+        <SumResult result={sum} textStyle={styles.texts}/>
       </View>
     </View>
   );
--- components/NumberBox.jsx
+++ components/NumberBox.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { TextInput, StyleSheet } from 'react-native'
 
-export default NumberBox = ({textStyle}) => {
+export default NumberBox = ({value, onChange, textStyle}) => {
   return (
     <TextInput
       style={[styles.inputText, textStyle]}
       keyboardType="numeric"
+      onChangeText={onChange}
+      value={value}
     />
   )
 }
